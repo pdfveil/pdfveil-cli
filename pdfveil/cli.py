1e0deca39a05a4a14866475d1de21503b5670804
Saku0512 <moonandstar118@gmail.com>
2025-04-05 00:26:57 +0900

    省略オプションを追加

--- pdfveil/cli.py
+++ pdfveil/cli.py
@@ -12,16 +12,16 @@ def run_cli():
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # 暗号化コマンド
-    encrypt_parser = subparsers.add_parser("encrypt", help="PDFを暗号化する")
-    encrypt_parser.add_argument("input", help="入力PDFファイルパス")
-    encrypt_parser.add_argument("--password", required=True, help="暗号化に使うパスワード")
-    encrypt_parser.add_argument("--output", help="保存先ファイル名（省略時: .veil.pdf）")
+    encrypt_parser = subparsers.add_parser("encrypt", aliases=["enc"], help="PDFを暗号化する")
+    encrypt_parser.add_argument("inputpdf", help="入力PDFファイルパス")
+    encrypt_parser.add_argument("-p" ,"--password", required=True, help="暗号化に使うパスワード")
+    encrypt_parser.add_argument("-o" ,"--output", help="保存先ファイル名（省略時: .veil.pdf）")
 
     # 復号コマンド
-    decrypt_parser = subparsers.add_parser("decrypt", help="PDFを復号する")
-    decrypt_parser.add_argument("input", help="暗号化されたファイル（.veil.pdf）")
-    decrypt_parser.add_argument("--password", required=True, help="復号に使うパスワード")
-    decrypt_parser.add_argument("--output", help="保存先ファイル名（省略時: .decrypted.pdf）")
+    decrypt_parser = subparsers.add_parser("decrypt", aliases=["enc"], help="PDFを復号する")
+    decrypt_parser.add_argument("veilpdf", help="暗号化されたファイル（.veil.pdf）")
+    decrypt_parser.add_argument("-p", "--password", required=True, help="復号に使うパスワード")
+    decrypt_parser.add_argument("-o" ,"--output", help="保存先ファイル名（省略時: .decrypted.pdf）")
 
     args = parser.parse_args()
 
